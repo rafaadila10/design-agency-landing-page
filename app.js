ceaeca133c52fa5ae02675c47fe18ac0cf0102f4
Mohammad Rafa Adila <mohammadrafa63@gmail.com>
2025-11-19 23:27:12 +0700

    create testimonial section

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   $('.menu-toggle').on('click', function() {
     $('.nav-links').toggleClass('active');
   });
@@ -6,4 +7,27 @@ $(document).ready(function() {
   $('.close-menu').on('click', function() {
     $('.nav-links').removeClass('active');
   });
+
+  $(".avatar").click(function () {
+      $(".avatar").removeClass("active");
+
+      $(this).addClass("active");
+
+      const text = $(this).data("text");
+      const name = $(this).data("name");
+      const role = $(this).data("role");
+
+      $(".testimonial-text").fadeOut(150, function () {
+          $(this).text(text).fadeIn(150);
+      });
+
+      $(".person-name").fadeOut(150, function () {
+          $(this).text(name).fadeIn(150);
+      });
+
+      $(".person-role").fadeOut(150, function () {
+          $(this).text(role).fadeIn(150);
+      });
+  });
+
 });
